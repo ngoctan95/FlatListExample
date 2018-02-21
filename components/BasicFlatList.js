fab8e74ea1b8c67d3f652cf1cc9c056f71071664
ngoctan95 <nguyenngoctan44@gmail.com>
2018-02-21 19:24:11 +0700

    Display data more and images

--- components/BasicFlatList.js
+++ components/BasicFlatList.js
@@ -4,16 +4,25 @@ import {
     View,
     Text,
     StyleSheet,
-    FlatList,List
+    FlatList,List,Image
 } from 'react-native';
 import flatListData from '../data/flatListData';
 
 class FlatListItem extends Component{
     render(){
         return(
-            <View style={{backgroundColor:this.props.index%2==0?'mediumseagreen':'tomato'}}>
-                <Text style={{backgroundColor:this.props.item.index%2==0?'tomato':'mediumseagreeen'}}>{this.props.item.name}</Text>
-                <Text>{this.props.item.des}</Text>
+            <View style={{
+                flex:1,
+                flexDirection:'row',
+                alignItems:'center',
+                backgroundColor:this.props.index%2==0?'mediumseagreen':'tomato'}}>
+                <Image source={{uri:this.props.item.link}}
+                    style={{width:100,height:100}}>
+                 </Image>
+                 <View style={{flex:1,flexDirection:'column',alignItems:'center'}}>
+                    <Text>{this.props.item.name}</Text>
+                    <Text>{this.props.item.des}</Text>
+                </View>
             </View>
         );
     }
--- data/flatListData.js
+++ data/flatListData.js
@@ -2,17 +2,20 @@ var basicData=[
     {
         "key":"1",
         "name":"First item",
-        "des":"This is the first"
+        "des":"This is the first",
+        "link":"https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/FIRST_Logo.svg/1180px-FIRST_Logo.svg.png"
     },
     {
         "key":"2",
         "name":"Second item",
-        "des":"This is the second"
+        "des":"This is the second",
+        "link":"https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/FIRST_Logo.svg/1180px-FIRST_Logo.svg.png"
     },
     {
         "key":"3",
         "name":"Third item",
-        "des":"This is the third"
+        "des":"This is the third.his is the third.his is the third.his is the third.his is the third.his is the third.his is the third.his is the third.his is the third.his is the third.his is the third.his is the third.his is the third.",
+        "link":"https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/FIRST_Logo.svg/1180px-FIRST_Logo.svg.png"
     }
 ];
 module.exports=basicData;
