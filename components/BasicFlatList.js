6e617ccc3a0d872a8c327b85a40866c7a880e52a
ngoctan95 <nguyenngoctan44@gmail.com>
2018-02-22 13:50:02 +0700

    Swipe for deleeting

--- components/BasicFlatList.js
+++ components/BasicFlatList.js
@@ -4,14 +4,51 @@ import {
     View,
     Text,
     StyleSheet,
-    FlatList,List,Image
+    FlatList,List,Image,Alert
 } from 'react-native';
 import flatListData from '../data/flatListData';
+import Swipeout from 'react-native-swipeout';
 
 class FlatListItem extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            activeRowKey:null
+        };
+    }
     render(){
+        const swipeSettings={
+            autoClose:true,
+            onClose: (secId,rowId,direction)=>{
+                
+            },
+            onOpen:(secId,rowId,direction)=>{
+
+            },
+            right:[
+                    {
+                        onPress:()=>{
+                            const deletingRow=this.state.activeRowKey;
+                            Alert.alert('Annoucement','Wanna delete',
+                        [
+                            {text:'No',onPress:()=>{console.log('Canceled')},style:'cancel'},
+                            {text:'OK',onPress:()=>{
+                                flatListData.splice(this.props.index,1);
+                                this.props.parentFlatList.refreshFlatList(deletingRow)}}
+                        ],
+                        {cancelable:true}
+                            )
+                        },
+                        text: 'Delete',type:'delete'
+                    }
+            ],
+            rowId:this.props.index,
+            sectionId:1
+        }
         return(
+            <Swipeout {...swipeSettings}>
             <View style={{
+            
                 flex:1,
                 flexDirection:'row',
                 alignItems:'center',
@@ -24,21 +61,36 @@ class FlatListItem extends Component{
                     <Text>{this.props.item.des}</Text>
                 </View>
             </View>
+            </Swipeout>
         );
     }
 }
 export default class BasicFlatList extends Component{
+    constructor(props){
+        super(props);
+        this.state=({
+            deletedRowKey:null,
+        });
+    }
+    refreshFlatList=(deleteKey)=>{
+        this.setState((prevState)=>{
+            return{
+                deletedRowKey:deleteKey
+            };
+        })
+    };
     render(){
         return(
-            <View>
+            <View style={{marginTop:20}}>
             
                 <FlatList 
+               
                 data ={flatListData}
                 renderItem={({item,index})=>{
                     console.log(`item=${JSON.stringify(item)}, index=${index}`)
                 return (
                     <FlatListItem item={item} 
-                        index={index}>
+                        index={index}  parentFlatList={this}>
                         </FlatListItem>
                 );
                 }}>
